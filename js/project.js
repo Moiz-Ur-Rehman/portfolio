812f07eb4b01eeda8ba8e893158f67c939f00250
Moiz-Ur-Rehman <moizurrehman0341@gmail.com>
2021-06-04 02:15:08 +0500

    animations updated

--- js/project.js
+++ js/project.js
@@ -1,40 +1,71 @@
+//card flip on hover
 CSSPlugin.defaultTransformPerspective = 1000;
 var cardback = document.querySelectorAll('.cardBack');
 TweenMax.set(cardback, { rotationY: -180 });
 var projectCards = document.querySelectorAll('.project-card');
-projectCards.forEach(cardFlip);
-function cardFlip(element, i) {
-    var frontCard = element.querySelectorAll(':scope > .cardFront');
-    var backCard = element.querySelectorAll(':scope > .cardBack');
-    tl = new TimelineMax({ paused: true });
-    tl
-        .to(frontCard, 1, { rotationY: 180 })
-        .to(backCard, 1, { rotationY: 0 }, 0)
-        .to(element, .5, { z: 50 }, 0)
-        .to(element, .5, { z: 0 }, .5);
-    element.animation = tl;
-};
-projectCards.forEach(cardHover);
-function cardHover(card) {
-    card.addEventListener('mouseenter', function () {
-        this.animation.play();
-        card.addEventListener('mouseleave', () => {
-            this.animation.reverse();
-        });
-    });
-};
+if (window.innerWidth > 600) {
+  projectCards.forEach(cardFlip);
+  function cardFlip(element, i) {
+      var frontCard = element.querySelectorAll(':scope > .cardFront');
+      var backCard = element.querySelectorAll(':scope > .cardBack');
+      tl = new TimelineMax({ paused: true });
+      tl
+          .to(frontCard, 1, { rotationY: 180 })
+          .to(backCard, 1, { rotationY: 0 }, 0)
+          .to(element, .5, { z: 50 }, 0)
+          .to(element, .5, { z: 0 }, .5);
+      element.animation = tl;
+  };
+
+  projectCards.forEach(cardHover);
+  function cardHover(card) {
+      card.addEventListener('mouseenter', function () {
+          this.animation.play();
+          card.addEventListener('mouseleave', () => {
+              this.animation.reverse();
+          });
+      });
+  };
+}
+//card skew on scroll
 let proxy = { skew: 0 },
-    skewSetter = gsap.quickSetter(".project-card", "skewY", "deg"), // fast
-    clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees. 
+    skewSetter = gsap.quickSetter(".project-card", "skewY", "deg"),
+    clamp = gsap.utils.clamp(-20, 20);
 ScrollTrigger.create({
   onUpdate: (self) => {
     let skew = clamp(self.getVelocity() / -300);
-    // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
     if (Math.abs(skew) > Math.abs(proxy.skew)) {
       proxy.skew = skew;
       gsap.to(proxy, {skew: 0, duration: 0.8, ease: "power3", overwrite: true, onUpdate: () => skewSetter(proxy.skew)});
     }
   }
 });
-// make the right edge "stick" to the scroll bar. force3D: true improves performance
 gsap.set(".project-card", {transformOrigin: "right center", force3D: true});
+const projects = document.querySelectorAll('.projects-full-screen');
+const fullscreenImage = document.querySelectorAll('.full-screen-image');
+const closeIcon = document.querySelectorAll('.close-icon');
+
+projectCards[0].addEventListener('click', () => {
+  projects[0].style.visibility = 'visible';
+  fullscreenImage[0].style.animation = 'slide 15s 2s infinite alternate ease-in-out';
+  closeIcon[0].addEventListener('click',()=>{
+    projects[0].style.visibility = 'hidden';
+    fullscreenImage[0].style.animation = 'none';
+  });
+});
+projectCards[1].addEventListener('click', () => {
+  projects[1].style.visibility = 'visible';
+  fullscreenImage[1].style.animation = 'slide 15s 2s infinite alternate ease-in-out';
+  closeIcon[1].addEventListener('click',()=>{
+    projects[1].style.visibility = 'hidden';
+    fullscreenImage[1].style.animation = 'none';
+  });
+});
+projectCards[2].addEventListener('click', () => {
+  projects[2].style.visibility = 'visible';
+  fullscreenImage[2].style.animation = 'slide 15s 2s infinite alternate ease-in-out';
+  closeIcon[2].addEventListener('click',()=>{
+    projects[2].style.visibility = 'hidden';
+    fullscreenImage[2].style.animation = 'none';
+  });
+});
